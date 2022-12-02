22087ab6ac8bf3cefb25dc3c77ec7e54c5eb4618
issufibadji <issufibadji@gmail.com>
2022-12-02 10:23:53 -0300

    add

---
+++ REACTJS/reactapp/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div>
+      <h1>LIsta de Presença</h1>
+      <font>
+        <input type="text" placeholder="Digite nome..." />
+        <button type="button">Adicionar</button>
+      </font>
+    </div>
+  )
+}
+
+export default Home
